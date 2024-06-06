43a601b2287a700d3dc5e3611454d049f2cc3423
AlfanMarcel <alfan.marcel2808@gmail.com>
2024-06-06 11:16:40 +0700

    try

--- bot.py
+++ bot.py
@@ -59,8 +59,7 @@ async def main() -> None:
 
     await application.initialize()
     await application.start()
-    await application.updater.start_polling()
-    await application.updater.idle()
+    await application.run_polling()
 
 if __name__ == "__main__":
     import asyncio
