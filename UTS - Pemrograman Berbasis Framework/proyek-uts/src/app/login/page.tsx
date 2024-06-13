b06b2a0fdada996960f5a9e054bc743ba37e128c
Lukman Hakim <madlukmanhakim@gmail.com>
2024-06-13 21:29:33 +0700

    minor update

--- UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/login/page.tsx
+++ UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/login/page.tsx
@@ -8,21 +8,24 @@ import { ReactDOM } from "react";
 import Link from "next/link";
 import Button from "@/components/atoms/button";
 import FormLogin from "@/components/organisms/FormLogin";
+import AuthLayout from "@/components/templates/AuthLayout";
 
 
 export default function Login() {
-  return (
-    <div className="flex justify-center items-center  min-h-screen bg-gradient-to-r from-gray-100 to-gray-300">
-      <div className="flex flex-col items-center bg-white p-10 rounded-lg shadow-lg max-w-sm w-full">
-        <div className="flex flex-col items-center">
-          <FormLogin />
-          <div className="py-2 text-gray-600">
-            <p>Belum punya akun? <Link href="/register" className="text-blue-500 hover:underline">Daftar dulu bos</Link></p>
-          
-          </div>
+    return (
+        <div className="flex justify-center items-center  min-h-screen bg-gradient-to-r from-gray-100 to-gray-300">
+            <div className="flex flex-col items-center bg-white p-10 rounded-lg shadow-lg max-w-sm w-full">
+                <div className="flex flex-col items-center">
+                    <AuthLayout title={"Login"}>
+                        <FormLogin />
+                    </AuthLayout>
+                    <div className="py-2 text-gray-600">
+                        <p>Belum punya akun? <Link href="/register" className="text-blue-500 hover:underline">Daftar dulu bos</Link></p>
+
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
--- UTS - Pemrograman Berbasis Framework/proyek-uts/src/components/molecules/InputForm.tsx
+++ UTS - Pemrograman Berbasis Framework/proyek-uts/src/components/molecules/InputForm.tsx
@@ -2,11 +2,11 @@ import Label from "../atoms/label";
 import Input from "../atoms/input";
 
 const InputForm = (props) => {
-    const {title, name, type, placeholder} = props;
+    const {title, name, type, placeholder, value} = props;
     return(
         <div className="my-3 text-black">
             <Label htmlFor={name}>{title}</Label>
-            <Input name={name} type={type} placeholder={placeholder} />
+            <Input name={name} type={type} placeholder={placeholder} value={value} />
         </div>
     );
 }
