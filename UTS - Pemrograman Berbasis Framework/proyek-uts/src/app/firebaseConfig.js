4626b364b232f961e86648c03c1b1e35600a5fb4
Lukman Hakim <madlukmanhakim@gmail.com>
2024-06-14 11:00:36 +0700

    update login

--- UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/firebaseConfig.js
+++ UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/firebaseConfig.js
@@ -1,6 +1,8 @@
 // firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHvf45coRDHnqMb3MhqE8E8vhYzPjEF2k",
@@ -10,10 +12,12 @@ const firebaseConfig = {
   storageBucket: "streamgyro-d4613.appspot.com",
   messagingSenderId: "7801719287",
   appId: "1:7801719287:web:5cb8fd2a6c39def2a8c6f2",
-  measurementId:"G-2M6L1E6Y1N"
+  measurementId: "G-2M6L1E6Y1N"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const log = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, db };
+export { app, db, auth, log };
--- UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/login/page.tsx
+++ UTS - Pemrograman Berbasis Framework/proyek-uts/src/app/login/page.tsx
@@ -1,19 +1,12 @@
-import EditProfile from "@/components/templates/EditProfile";
-import Account from "@/app/account/page";
-import Dashboard from "@/app/dashboard/page";
-import Register from "@/app/register/page";
-import Setting from "@/app/setting/page";
+// Login.js
 import React from "react";
-import { ReactDOM } from "react";
 import Link from "next/link";
-import Button from "@/components/atoms/button";
 import FormLogin from "@/components/organisms/FormLogin";
 import AuthLayout from "@/components/templates/AuthLayout";
 
-
 export default function Login() {
     return (
-        <div className="flex justify-center items-center  min-h-screen bg-gradient-to-r from-gray-100 to-gray-300">
+        <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-gray-100 to-gray-300">
             <div className="flex flex-col items-center bg-white p-10 rounded-lg shadow-lg max-w-sm w-full">
                 <div className="flex flex-col items-center">
                     <AuthLayout title={"Login"}>
@@ -21,11 +14,9 @@ export default function Login() {
                     </AuthLayout>
                     <div className="py-2 text-gray-600">
                         <p>Belum punya akun? <Link href="/register" className="text-blue-500 hover:underline">Daftar dulu bos</Link></p>
-
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
