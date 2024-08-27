0c1d584e4ed40fd51c836eab6af6e06b8e4f9056
AlfanMarcel <alfan.marcel2808@gmail.com>
2024-08-27 16:24:36 +0700

    las

--- match.py
+++ match.py
@@ -33,45 +33,6 @@ class_labels = [
     "Lateral fall when trying to sit down"
 ]
 
-# class_labels = [
-#     "Stand for 30 seconds (D01/01)",
-#     "Stand, slowly bend the back with or without bending at knees, tie shoe lace, and get up (D02/02)",
-#     "Pick up an object from the floor (D03/03)",
-#     "Gently jump (try to reach an object) (D04/04)",
-#     "Stand, sit to the ground, wait a moment, and get up with normal speed (D05/05)",
-#     "Walk normally with turn (4m) (D06/06)",
-#     "Walk quickly with turn (4m) (D07/07)",
-#     "Jog normally with turn (4m) (D08/08)",
-#     "Jog quickly with turn (4m) (D09/09)",
-#     "Stumble while walking (D10/10)",
-#     "Sit on a chair for 30 seconds (D11/11)",
-#     "Sit on the sofa (back is inclined to the support) for 30 seconds (D12/12)",
-#     "Sit down to a chair normally, and get up from a chair normally (D13/13)",
-#     "Sit down to a chair quickly, and get up from a chair quickly (D14/14)",
-#     "Sit a moment, trying to get up, and collapse into a chair (D15/15)",
-#     "Stand, sit on the sofa (back is inclined to the support), and get up normally (D16/16)",
-#     "Lie on the bed for 30 seconds (D17/17)",
-#     "Sit a moment, lie down to the bed normally, and get up normally (D18/18)",
-#     "Sit a moment, lie down to the bed quickly, and get up quickly (D19/19)",
-#     "Walk upstairs and downstairs normally (5 steps) (D20/35)",
-#     "Walk upstairs and downstairs quickly (5 steps) (D21/36)",
-#     "Forward fall when trying to sit down (F01/20)",
-#     "Backward fall when trying to sit down (F02/21)",
-#     "Lateral fall when trying to sit down (F03/22)",
-#     "Forward fall when trying to get up (F04/23)",
-#     "Lateral fall when trying to get up (F05/24)",
-#     "Forward fall while sitting, caused by fainting (F06/25)",
-#     "Lateral fall while sitting, caused by fainting (F07/26)",
-#     "Backward fall while sitting, caused by fainting (F08/27)",
-#     "Vertical (forward) fall while walking caused by fainting (F09/28)",
-#     "Fall while walking, use of hands to dampen fall, caused by fainting (F10/29)",
-#     "Forward fall while walking caused by a trip (F11/30)",
-#     "Forward fall while jogging caused by a trip (F12/31)",
-#     "Forward fall while walking caused by a slip (F13/32)",
-#     "Lateral fall while walking caused by a slip (F14/33)",
-#     "Backward fall while walking caused by a slip (F15/34)"
-# ]
-
 def predict_and_save():
     # Muat data JSON dari file
     with open('sensordata.json', 'r') as f:
